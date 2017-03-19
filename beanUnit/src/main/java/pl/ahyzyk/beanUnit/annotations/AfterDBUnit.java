92a3926d55ab88249ba79a6042ae27061cf6bcb8
domus <domus@konto.pl>
2017-03-19 10:10:43 +0100

    some fixes

--- beanUnit/src/main/java/pl/ahyzyk/beanUnit/annotations/AfterDBUnit.java
+++ beanUnit/src/main/java/pl/ahyzyk/beanUnit/annotations/AfterDBUnit.java
@@ -1,13 +1,15 @@
 package pl.ahyzyk.beanUnit.annotations;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Created by andrz on 17.03.2017.
  */
-@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-@Inherited
+@Target({ElementType.METHOD})
 /**
  * Executed after dbUnit testing ex. @ShouldMatchDataSet
  *
